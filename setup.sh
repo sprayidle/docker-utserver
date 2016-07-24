56ed125a70d5b1df0aff2c88319208a4c8447481
sprayidle <sprayidle@gmail.com>
2016-07-24 20:59:30 +1200

    add .conf file

--- setup.sh
+++ setup.sh
@@ -4,3 +4,8 @@
 cp -n /opt/utorrent-server/webui.zip /config/webui.zip
 rm /opt/utorrent-server/webui.zip
 ln -s /config/webui.zip /opt/utorrent-server/webui.zip
+
+# Copy .conf file to config folder and link back to /opt/utorrent-server
+cp -n /opt/utorrent-server/utserver.conf /config/utserver.conf
+rm /opt/utorrent-server/utserver.conf
+ln -s /config/utserver.conf /opt/utorrent-server/utserver.conf
