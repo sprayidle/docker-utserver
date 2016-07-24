2ff7a6211fbabab870041697f87f6dbd5cb89eee
sprayidle <sprayidle@gmail.com>
2016-07-24 20:57:27 +1200

    Finish setup.sh comment

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copy webui to config folder and link back to 
+# Copy webui to config folder and link back to /opt/utorrent-server
 cp -n /opt/utorrent-server/webui.zip /config/webui.zip
 rm /opt/utorrent-server/webui.zip
 ln -s /config/webui.zip /opt/utorrent-server/webui.zip
