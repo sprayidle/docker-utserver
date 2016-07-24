e898c496c24e8d1fc4d966805f17c506d9e0311b
sprayidle <sprayidle@gmail.com>
2016-07-25 10:12:13 +1200

    Move config file setup to Dockerfile

--- setup.sh
+++ setup.sh
@@ -1,10 +1,1 @@
 #!/bin/bash
-
-# Copy webui to config folder and link back to /opt/utorrent-server
-cp -n /opt/utorrent-server/webui.zip /config/webui.zip
-rm /opt/utorrent-server/webui.zip
-ln -s /config/webui.zip /opt/utorrent-server/webui.zip
-
-# Copy .conf file to config folder and link back to /opt/utorrent-server
-cp -n /tmp/utserver.conf /config/utserver.conf
-rm /tmp/utserver.conf
